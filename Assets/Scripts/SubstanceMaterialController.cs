8f25d8a2133d73662b52e578739525c45e4a98f9
haneda-atsushi <hanecci@gmail.com>
2017-09-17 03:49:54 +0900

    Add demo.gif

--- Assets/Scripts/SubstanceMaterialController.cs
+++ Assets/Scripts/SubstanceMaterialController.cs
@@ -2,19 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
-
-#if UNITY_EDITOR
 using UnityEditor;
-#endif
 
 public class SubstanceMaterialController : MonoBehaviour
 {
     Renderer             m_Renderer;
-    ProceduralMaterial[] m_ProcMaterialArray;
+    List<ProceduralMaterial> m_ProcMaterialList = new List<ProceduralMaterial>();
 
     void Start ()
     {
         m_Renderer = GetComponent<Renderer>();
+
+        Object[] obj_array = Resources.LoadAll( "SubstanceArch", typeof( UnityEditor.SubstanceArchive ) );
+        foreach ( Object obj in obj_array )
+        {
+            SubstanceArchive sbs               = obj as SubstanceArchive;
+            // https://forum.unity.com/threads/substance-proceduralmaterial-to-material-possible.200767/
+            string fromPath                    = AssetDatabase.GetAssetPath( sbs.GetInstanceID() );
+            SubstanceImporter fromImporter     = AssetImporter.GetAtPath( fromPath ) as SubstanceImporter;
+            int fromMaterialCount              = fromImporter.GetMaterialCount();
+            ProceduralMaterial[] fromMaterials = fromImporter.GetMaterials();
+
+            foreach ( ProceduralMaterial proc in fromMaterials )
+            {
+                m_ProcMaterialList.Add( proc );
+            }
+        }
+        Debug.Log( "m_ProcMaterialArray.Length = " + m_ProcMaterialList.Count );
+
+        /*
         m_ProcMaterialArray =
             Resources.FindObjectsOfTypeAll( typeof( ProceduralMaterial ) ) as ProceduralMaterial[];
         Debug.Log( "m_ProcMaterialArray.Length = " + m_ProcMaterialArray.Length );
@@ -22,24 +38,14 @@ public class SubstanceMaterialController : MonoBehaviour
         {
             Debug.Log( "proc_mat.name = " + proc_mat.name );
         }
-        StartCoroutine( "ChangeProcMaterial" );
+        */
 
         /*
         m_Texture = 
             Resources.Load("ground_rock_face/resources/ground_rock_face_basecolor") as Texture2D;
         Debug.Log( "m_Texture.name = " + m_Texture.name );
         */
-
-        /*
-#if UNITY_EDITOR
-        obj_array = Resources.LoadAll( "SubstanceArch", typeof( UnityEditor.SubstanceArchive ) );
-        foreach ( Object obj in obj_array )
-        {
-            SubstanceArchive sbs = obj as SubstanceArchive;
-            Debug.Log( "sbs = " + sbs.name );
-        }
-#endif
-        */
+        StartCoroutine( "ChangeProcMaterial" );
     }
 	
 	void Update()
@@ -48,7 +54,9 @@ public class SubstanceMaterialController : MonoBehaviour
 
     private IEnumerator ChangeProcMaterial()
     {
-        foreach( ProceduralMaterial proc_mat in m_ProcMaterialArray )
+        // yield return new WaitForSeconds( 1.0f );
+
+        foreach( ProceduralMaterial proc_mat in m_ProcMaterialList )
         {
             Debug.Log( "Set material to" + proc_mat.name );
             m_Renderer.sharedMaterial = proc_mat;
